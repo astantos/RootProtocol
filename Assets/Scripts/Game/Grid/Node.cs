2ca37f61110704a0a7a2933cce7c221988074f6f
astantos <jimxu9000plus@gmail.com>
2023-02-04 02:38:29 +1100

    Added color changing to the particle systems.

--- Assets/Scripts/Game/Grid/Node.cs
+++ Assets/Scripts/Game/Grid/Node.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MainModule = UnityEngine.ParticleSystem.MainModule;
 
 public class Node : MonoBehaviour
 {
@@ -19,6 +20,11 @@ public class Node : MonoBehaviour
     public Direction Left;
     public Direction Right;
 
+    [Header("Colors")]
+    public Color NeutralColor;
+    public Color PlayerOneColor;
+    public Color PlayerTwoColor;
+
     [Header("References")]
     public GameObject Neutral;
     public GameObject PlayerOne;
@@ -29,5 +35,29 @@ public class Node : MonoBehaviour
         Neutral.SetActive(state == State.Neutral);
         PlayerOne.SetActive(state == State.P1);
         PlayerTwo.SetActive(state == State.P2);
+        UpdateColor(state);
+    }
+
+    protected void UpdateColor(State state)
+    {
+        Color color;
+        switch (state)
+        {
+            case State.P1: color = PlayerOneColor; break;
+            case State.P2: color = PlayerTwoColor; break;
+            default: color = NeutralColor; break;
+        }
+
+        MainModule upMain = Up.particles.main;
+        upMain.startColor = color;
+
+        MainModule downMain = Down.particles.main;
+        downMain.startColor = color;
+
+        MainModule leftMain = Left.particles.main;
+        leftMain.startColor = color;
+        
+        MainModule rightMain = Right.particles.main;
+        rightMain.startColor = color;
     }
 }
