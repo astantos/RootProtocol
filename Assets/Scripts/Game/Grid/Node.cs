e9bbd8fab528a3bfda0a0b3f640db17ba027243f
astantos <jimxu9000plus@gmail.com>
2023-02-04 09:18:09 +1100

    Added code for particle system to indicate when a Node has been selected.

--- Assets/Scripts/Game/Grid/Node.cs
+++ Assets/Scripts/Game/Grid/Node.cs
@@ -19,6 +19,8 @@ public class Node : MonoBehaviour
     public Direction Down;
     public Direction Left;
     public Direction Right;
+    public ParticleSystem P1Selected;
+    public ParticleSystem P2Selected;
 
     [Header("Colors")]
     public Color NeutralColor;
@@ -29,6 +31,15 @@ public class Node : MonoBehaviour
     public GameObject Neutral;
     public GameObject PlayerOne;
     public GameObject PlayerTwo;
+
+    private void Start()
+    {
+        MainModule p1main = P1Selected.main;
+        p1main.startColor = PlayerOneColor;
+        
+        MainModule p2main = P2Selected.main;
+        p2main.startColor = PlayerTwoColor;
+    }
 
     public void SetState(State state)
     {
@@ -59,5 +70,19 @@ public class Node : MonoBehaviour
         
         MainModule rightMain = Right.particles.main;
         rightMain.startColor = color;
+    }
+
+    protected void SetSelected(State state, bool selected)
+    {
+        if (state == State.P1)
+        {
+            if (selected) P1Selected.Play();
+            else P1Selected.Stop();
+        }
+        else if (state == State.P2)
+        {
+            if (selected) P2Selected.Play();
+            else P2Selected.Stop();
+        }
     }
 }
